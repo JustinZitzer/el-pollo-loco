774648077067d07b427de0a460aa4f824811eb9c
JustinZitzer <justinzi@web.de>
2026-05-22 14:48:01 +0200

    add error handling for image rendering

--- models/drawable-object.class.js
+++ models/drawable-object.class.js
@@ -13,7 +13,13 @@ class DrawableObject {
     }
 
     draw(ctx) {
+        try {
         ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+        } catch (error) {
+            console.warn('Error loading image', error);
+            console.log('Could not load image', this.img.scr);
+            
+        }
     }
 
     drawFrame(ctx) {
