fdb32bda29382432bf1d5bf976ac847b0f6a277f
JustinZitzer <justinzi@web.de>
2026-05-25 20:30:23 +0200

    implemented images for rotating salsa bottle and added animate function

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,17 +1,21 @@
 class ThrowableObect extends MovableObject {
 
     IMAGES_SALSA_BOTTLE = [
-
+        'img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png',
     ]
 
     constructor(x, y) {
         super().loadImage('img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png');
-        //this.loadImages(this.IMAGES_SALSA_BOTTLE);
+        this.loadImages(this.IMAGES_SALSA_BOTTLE);
         this.x = x - 60;
         this.y = y - 30;
         this.height = 100;
         this.width = 60;
         this.throw();
+        this.animate();
     }
 
     throw() {
@@ -20,6 +24,13 @@ class ThrowableObect extends MovableObject {
         setInterval(() => {
             this.x += 10;
         }, 25);
+    }
+
+    animate() {
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_SALSA_BOTTLE);
+        }, 100);
+    
     }
 
 
