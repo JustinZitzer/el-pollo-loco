a92853db6620dc924cc516b1dead8ff55879859b
JustinZitzer <justinzi@web.de>
2026-06-11 20:13:06 +0200

    temporary excluded blue hotbox frame

--- models/world.class.js
+++ models/world.class.js
@@ -197,7 +197,7 @@ class World {
             this.flipImage(movableObject);
         }
         movableObject.draw(this.ctx);
-        movableObject.drawFrame(this.ctx);
+        // movableObject.drawFrame(this.ctx);
         movableObject.drawRedFrame(this.ctx);
         if (movableObject.otherDirection) {
             this.flipImageBack(movableObject);
