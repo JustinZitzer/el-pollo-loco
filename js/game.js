c9f039f9eb13bcd681ee810e7215055c967760ae
JustinZitzer <justinzi@web.de>
2026-07-28 21:17:17 +0200

    deleted start game canvas onclick

--- js/game.js
+++ js/game.js
@@ -2,7 +2,6 @@ let canvas;
 let world;
 let keyboard = new Keyboard();
 
-document.getElementById('canvas').addEventListener('click', init); // start canvas onclick level
 
 function init() {
     canvas = document.getElementById('canvas');
